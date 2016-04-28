ffbb21aeec38fb0a60b5d7a0434168ee328a6095
Roy Ratcliffe <roy@pioneeringsoftware.co.uk>
2016-04-28 12:07:57 +0100

    Filter operation dependencies for cancelled ops

---
+++ AdvancedOperations/NSOperation+AdvancedOperations.swift
@@ -0,0 +1,39 @@
+// AdvancedOperations NSOperation+AdvancedOperations.swift
+//
+// Copyright © 2016, Roy Ratcliffe, Pioneering Software, United Kingdom
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the “Software”), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in
+//  all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED “AS IS,” WITHOUT WARRANTY OF ANY KIND, EITHER
+// EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT. IN NO
+// EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES
+// OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+// ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+// DEALINGS IN THE SOFTWARE.
+//
+//------------------------------------------------------------------------------
+
+import Foundation
+
+extension NSOperation {
+
+  /// - returns: All cancelled dependencies, or an empty array of operations if
+  ///   no dependencies or none cancelled. This is useful to know when you want
+  ///   to make an operation only continue if all its dependences finished
+  ///   without cancellation.
+  public var cancelledDependencies: [NSOperation] {
+    return dependencies.filter { (op) -> Bool in
+      return op.cancelled
+    }
+  }
+
+}
